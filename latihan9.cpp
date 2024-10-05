cbff3fd9cb208886ee92a19a74de5d6b129c56c9
ariyogaadmaja <ariyoga939@gmail.com>
2024-10-05 12:48:12 +0700

    Add files via upload
    tugas pak jakriya,tggl 4 okteber 2024

---
+++ latihan9.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main () { 
+float harga,jumlahjual,totalharga,diskon,totalbayar;
+char nama[20];
+cout<< "TOKO BAJU"<<endl;
+cout<<"nama barang: ";
+cin>>nama;
+cout<<"harga : ";
+cin>>harga;
+cout<<"jumlah jual : ";
+cin>>jumlahjual;
+
+cout<<setprecision(13);
+totalharga =harga*jumlahjual;
+cout<<"total harga : "<<totalharga<<endl;
+
+diskon=totalharga*0.10;
+cout<<"diskon : "<<diskon<<endl;
+
+totalbayar=totalharga-diskon;
+cout<<"total harga : "<<totalbayar<<endl;
+
+     cout<<"total barang yang terjual di toko adalah : "<<totalharga <<endl;
+     cout<<"apakah mau lanjut input ? (Y/T) : ";
+     cout<<endl;
+
+return 0;
+  
+}
+	   
+ 
+
+
+	
+	
+
